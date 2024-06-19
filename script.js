74ee7e0a26af96e6886ce5b40d677aab9d250f25
Beaprofessor <professornberlin2122@gmail.com>
2024-06-19 10:07:23 +0530

    Making the paddle move

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let table = document.getElementById("ping-pong-table");
   let ball = document.getElementById("ball"); // targetting the ball element
+  let paddle = document.getElementById("paddle"); // targetting the paddle
 
   // Here the ballX and ballY will be helping us to set a starting point of ball w.r.t table
-  let ballX = 10; // distance of the top of the ball w.r.t ping pong table
-  let ballY = 10; // distance of the left of the ball w.r.t ping pong table
+  let ballX = 50; // distance of the top of the ball w.r.t ping pong table
+  let ballY = 50; // distance of the left of the ball w.r.t ping pong table
 
   let dx = 2; // displacement factor in x-direction  here 2 means - > you will displace by 2px in +x direction and -2 means -> you will displace by 2px in -x direction
   let dy = 2; // displacement factor in x-direction  here 2 means - > you will displace by 2px in +y direction and -2 means -> you will displace by 2px in -y direction
@@ -15,15 +17,28 @@ document.addEventListener("DOMContentLoaded", () => {
     ballX += dx;
     ballY += dy;
 
-
     ball.style.left = `${ballX}px`;
     ball.style.top = `${ballY}px`;
 
     // if (ballX > 675 || ballX <= 0) dx *= -1;
     // if (ballY > 375 || ballY <= 0) dy *= -1;
 
-    if (ballX > table.offSetWidth  - ball.offsetWidth  || ballX <= 0) dx *= -1; // change the x-direction
-    if (ballY < table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1; // change the y-direction
-
+    if (ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1; // change the x-direction
+    if (ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1; // change the y-direction
   }, 1);
+
+  let paddleY = 0;
+  let dpY = 5;
+
+  document.addEventListener("keydown", (event) => {
+    if (event.keyCode == 38 && paddleY >0) {
+      //up arrow
+      paddleY += -1*dpY;
+      console.log("up", paddleY);
+    } else if (event.keyCode == 40 && paddleY < table.offsetHeight - paddle.offsetHeight) {
+      // down arrow
+      paddleY += dpY;
+    }
+    paddle.style.top = `${paddleY}px`;
+  });
 });
